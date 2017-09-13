574f87e947533ce2b925733c23eac53232a778b2
amreshtech <amreshtech@gmail.com>
2017-09-13 22:43:37 +0530

    null

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { CompetitionComponent } from './competition/competition.component';
+import { StandingsComponent } from './standings/standings.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompetitionComponent
+    CompetitionComponent,
+    StandingsComponent,
+    TeamsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
